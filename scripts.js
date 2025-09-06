ae50fb63f620dd77077f15c65cd3a2cecf0dd634
gilciccio94-lab <gilciccio94@gmail.com>
2025-09-06 09:03:51 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = document.querySelector('.search input');
-  searchInput.addEventListener('input', () => {
-    console.log('Ricerca:', searchInput.value);
-  });
-});
