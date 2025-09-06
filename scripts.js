e92e18063e356b6c72e867e55f3c89b249907a25
gilciccio94-lab <gilciccio94@gmail.com>
2025-09-06 08:58:47 +0200

    Primo upload del sito

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.querySelector('.search input');
+  searchInput.addEventListener('input', () => {
+    console.log('Ricerca:', searchInput.value);
+  });
+});
